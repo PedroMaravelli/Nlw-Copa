99f875b0710bedeea2e3c8762c2ee4d5085dc2b4
PedroMaravelli <phmaravelli123@gmail.com>
2022-11-01 17:30:02 -0300

    add models in db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import Fastify from "fastify";
+import {PrismaClient} from '@prisma/client'
+import Cors from "@fastify/cors";
 
+const prisma = new PrismaClient({
+    log: ['query']
+})
 async function bootstrap(){
     const fastify = Fastify({
         logger: true,
     })
+    await fastify.register(Cors,{
+        origin: true,
+    })
 
-    await fastify.listen({port: 5000})
-
+    fastify.get('/pools/count', async (req, res) =>{
+        const count = await prisma.poll.count()
+        res.send(count)
+    })
+    
+    await fastify.listen({port: 5000, host: '0.0.0.0'})
+    
 }
 bootstrap()
