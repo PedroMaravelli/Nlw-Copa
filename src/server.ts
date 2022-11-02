73f4ef131c22a913c2cf0abde962960ccca76b41
PedroMaravelli <phmaravelli123@gmail.com>
2022-11-02 18:34:42 -0300

    insert data in db

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import {PrismaClient} from '@prisma/client'
 import Cors from "@fastify/cors";
+import z from 'zod'
 
 const prisma = new PrismaClient({
     log: ['query']
@@ -16,6 +17,19 @@ async function bootstrap(){
     fastify.get('/pools/count', async (req, res) =>{
         const count = await prisma.poll.count()
         res.send(count)
+    })
+
+
+    fastify.post('/pools', async (req, res)=>{
+        const createPoolTitle = z.object({
+            title: z.string()
+        })
+
+        const {title} = createPoolTitle.parse(req.body)
+        
+
+        return res.status(201).send({title})
+
     })
     
     await fastify.listen({port: 5000, host: '0.0.0.0'})
